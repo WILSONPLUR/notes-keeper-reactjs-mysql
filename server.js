69b4b9967c9372c8cf001e00102effc2cba531c4
WILSONPLUR <yourshows12@gmail.com>
2023-02-26 23:21:36 +0300

    added new methods in api

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3500;
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/get", (req, res) => {
+app.get("/api/get/notes", (req, res) => {
   db.query("SELECT * FROM notes_data", (err, result) => {
     if (err) {
       console.log(err);
@@ -16,18 +16,61 @@ app.get("/api/get", (req, res) => {
   });
 });
 
-app.post("/api/post", (req, res) => {
+app.post("/api/post/note", (req, res) => {
   db.query(
     `INSERT INTO notes_data (
         title,
         description,
-        isDone
+        isDone,
+        _id
     ) VALUES (
         ?,
         ?,
-        ?
+        ?,
+        UUID()
     )`,
     [req.body.title, req.body.description, req.body.isDone],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(result);
+    }
+  );
+});
+
+app.put("/api/update/note/:id", (req, res) => {
+  const id = req.params.id;
+  db.query(
+    `
+  UPDATE notes_data 
+  SET title = ?, description = ?, isDone = ?
+  WHERE _id = ${id}`,
+    [req.body.title, req.body.description, req.body.isDone],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(result);
+    }
+  );
+});
+
+app.delete("/api/delete/note/:id", (req, res) => {
+  const id = req.params.id;
+  db.query(`DELETE FROM notes_data WHERE _id = '${id}'`, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
+
+app.delete("/api/delete/notes", (req, res) => {
+  db.query(
+    `
+    DELETE FROM notes_data;
+  `,
     (err, result) => {
       if (err) {
         console.log(err);
